61d140aa818cd9447ed05d59983a624405731b60
James Noble <kjx@ecs.vuw.ac.nz>
2024-11-03 16:22:30 +1300

    add scripts

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+
+time nightly verify dfyconfig.toml --log-format "text;LogFileName=CCC-log.txt"  --filter-symbol=Clone_Clone_Clone; 
+time nightly verify dfyconfig.toml --log-format "text;LogFileName=KKK-log.txt"  --filter-symbol=Klone_Klone_Klone; 
+time nightly verify dfyconfig.toml --log-format "text;LogFileName=Clog.txt"  --filter-symbol=Clone; 
+time nightly verify dfyconfig.toml --log-format "text;LogFileName=Klog.txt"  --filter-symbol=Klone; 
+time nightly verify dfyconfig.toml --log-format "text;LogFileName=log.txt" ;
+
+
